5d4fad4fc399e2fd6778ea78f979eea7313d2c91
Roberta Tatiana <75459393+robertatatiana@users.noreply.github.com>
2021-09-15 23:31:00 -0300

    Add files via upload

---
+++ CatalogoDeJogosAPI/CatalogoDeJogosAPI/Controllers/V1/JogosController.cs
@@ -0,0 +1,108 @@
+﻿using CatalogoDeJogosAPI.Excepitions;
+using CatalogoDeJogosAPI.InputModel;
+using CatalogoDeJogosAPI.Services;
+using CatalogoDeJogosAPI.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogoDeJogosAPI.Controllers.V1
+{
+    [Route("api/V1/[controller]")]
+    [ApiController]
+    public class JogosController : ControllerBase
+    {
+        private readonly IJogoService _jogoservice;
+
+        public JogosController(IJogoService jogoservice)
+        {
+            _jogoservice = jogoservice;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<JogoViewModel>>> Obter([FromQuery, Range(1, int.MaxValue)] int pagina = 1, [FromQuery, Range(1, 50)] int quantidade = 5)
+        {
+            var jogos = await _jogoservice.Obter(pagina, quantidade);
+
+            if (jogos.Count() == 0)
+                return NoContent();
+
+            return Ok(jogos);
+        }
+
+        [HttpGet("{idJogo:guid}")]
+        public async Task<ActionResult<JogoViewModel>> Obter([FromRoute] Guid idJogo)
+        {
+            var jogo = await _jogoservice.Obter(idJogo);
+
+            if (jogo == null)
+                return NoContent();
+
+            return Ok(jogo);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<JogoViewModel>> InserirJogo([FromBody] JogoInputModel jogoInputModel)
+        {
+            try
+            {
+                var jogo = await _jogoservice.Inserir(jogoInputModel);
+
+                return Ok(jogo);
+            }
+            catch (JogoJaCadastradoException ex)
+            {
+                return UnprocessableEntity("Já existe um jogo com este nome para esta produtora");
+            }
+        }
+
+        [HttpPut("{idJogo:guid}")]
+        public async Task<ActionResult> AtualizarJogo([FromRoute] Guid idJogo, [FromBody] JogoInputModel jogoInputModel)
+        {
+            try
+            {
+                await _jogoservice.Atualizar(idJogo, jogoInputModel);
+
+                return Ok();
+            }
+            catch (JogoJaCadastradoException ex)
+            {
+                return NotFound("Não existe este jogo");
+            }
+        }
+
+        [HttpPatch("{idJogo:guid}/preco/{preco:double}")]
+        public async Task<ActionResult> AtualizarJogo([FromRoute] Guid idJogo, [FromRoute] double preco)
+        {
+            try
+            {
+                await _jogoservice.Atualizar(idJogo, preco);
+
+                return Ok();
+            }
+            catch (JogoJaCadastradoException ex)
+            {
+                return NotFound("Não existe este jogo");
+            }
+        }
+
+        [HttpDelete("{idJogo:guid}")]
+        public async Task<ActionResult> ApagarJogo([FromRoute] Guid idJogo)
+        {
+            try
+            {
+                await _jogoservice.Remover(idJogo);
+
+                return Ok();
+            }
+            catch (JogoJaCadastradoException ex)
+            {
+                return NotFound("Não existe este jogo");
+            }
+        }
+
+    }
+}
